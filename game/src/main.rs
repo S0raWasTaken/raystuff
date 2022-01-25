c70497aa088a4f807f0a3123d6159645ae661b7d
S0raWasTaken <s0ra@duck.com>
2022-01-25 02:16:06 -0500

    Rename obj to player

--- game/src/main.rs
+++ game/src/main.rs
@@ -25,14 +25,14 @@ async fn main() -> Result<(), Error> {
         timings(timing_zero, rx).await;
     });
 
-    let mut obj = Movement {
+    let mut player = Movement {
         object: Square {
             pos_x: 30,
             pos_y: 30,
             size: 20,
             color: Color::WHITE,
         },
-        speed_x: 0,
+        speed_x: 1,
         speed_y: 0,
     };
 
@@ -43,15 +43,18 @@ async fn main() -> Result<(), Error> {
         draw_handle.draw_text("heya", 800 / 2 - 20, 600 / 64, 20, Color::WHITE);
 
         draw_handle.draw_line(800 / 16, 600 / 16, 800 - 800 / 16, 600 / 16, Color::WHITE);
-        obj.tick(&mut draw_handle);
+        player.tick(&mut draw_handle);
 
         tx.send(()).map_err(Error::TxSend)?;
+
     }
     exit(0);
 }
 
 async fn timings(zero: Instant, rx: Receiver<()>) {
     let mut ticks: u128 = 0;
+
+    println!();
 
     loop {
         rx.recv().unwrap_or(());
